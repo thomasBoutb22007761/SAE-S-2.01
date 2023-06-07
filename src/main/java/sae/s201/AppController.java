ebdd3b4780f1805ad8fb4d80809786e1e293d8fe
root <root@chips.localdomain>
2023-06-07 18:53:39 +0200

    fxml

---
+++ src/main/java/sae/s201/AppController.java
@@ -0,0 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+public class AppController {
+
+    @FXML
+    private Slider dateSlider;
+
+    @FXML
+    private Label taDateLabel;
+
+    @FXML
+    public void initialize() {
+        // Ajoutez ici le code d'initialisation du contrôleur
+        dateSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            // Mise à jour de la date sélectionnée dans le label "taDateLabel"
+            taDateLabel.setText(newValue.toString());
+        });
+    }
+}
